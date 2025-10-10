269b0cd4af9ee28b039d834a548ecf4d5f0b064a
tamojit <tamojitbiswas176@gmail.com>
2025-10-10 09:23:42 +0530

    change in navbar

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ passport.deserializeUser(User.deserializeUser());
 app.use((req, res, next) => {
   res.locals.success = req.flash('success');
   res.locals.error = req.flash('error');
-  res.locals.currentUser = req.user; // changed from curr to currentUser
+  res.locals.curr = req.user;
   next();
 });
 
